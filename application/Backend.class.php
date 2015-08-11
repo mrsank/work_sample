fc9def8b391c599f49d5faeb295a70cf2206bbf3
Harisankar <mrsank@gmail.com>
2015-08-11 22:48:14 +0530

    Updated code comments

--- application/Backend.class.php
+++ application/Backend.class.php
@@ -23,50 +23,45 @@ class Backend extends Controller  {
         $this->render('admin');
     }
 
+
+    /*
+      * @ Modified : Harisankar <mrsank@live.in>
+      * @ Date : 08/11/2015
+      * @ Purpose : Generation of XML file
+      */
+
     public function getAllData(){
 
         $aProductInfo = $this->_getProductReposirty()->getAllData();
 
         header( "content-type: application/xml; charset=ISO-8859-15" );
-
         $xml = new DOMDocument( "1.0", "ISO-8859-15" );
 
         $xml_products = $xml->createElement( "products" );
-
         foreach ($aProductInfo as $value) {
-        $xml_product = $xml->createElement( "product" );
 
+            $xml_product = $xml->createElement( "product" );
+            $xml_name = $xml->createElement( "name", $value->name);
+            $xml_product->appendChild( $xml_name );
+            $xml_price = $xml->createElement( "price", $value->cost);
+            $xml_product->appendChild( $xml_price );
+            $xml_description = $xml->createElement( "description", $value->description);
+            $xml_product->appendChild( $xml_description );
+            $xml_category = $xml->createElement( "category", $value->catname);
+            $xml_product->appendChild( $xml_category );
 
-        $xml_name = $xml->createElement( "name", $value->name);
-        $xml_product->appendChild( $xml_name );
-
-        $xml_price = $xml->createElement( "price", $value->cost);
-        $xml_product->appendChild( $xml_price );
-
-        $xml_description = $xml->createElement( "description", $value->description);
-        $xml_product->appendChild( $xml_description );
-
-        $xml_category = $xml->createElement( "category", $value->catname);
-        $xml_product->appendChild( $xml_category );
-
-        $id = $value->id;
-        $attribute = $this->_getProductReposirty()->getAttribute($id);
-        $xml_attribute = $xml->createElement( "attributes" );
-        foreach ($attribute as $attr) {
-            $xml_attri = $xml->createElement( "attribute", $attr->name);
-            $xml_attribute->appendChild( $xml_attri );
-            $attri = $attr->name;
-        }
-        $xml_product->appendChild( $xml_attribute );
-        
-
-
-        $xml_products->appendChild( $xml_product );
-
-        
-        $xml_products->appendChild( $xml_product );
-
-        $xml->appendChild( $xml_products );
+            $id = $value->id;
+            $attribute = $this->_getProductReposirty()->getAttribute($id);
+            $xml_attribute = $xml->createElement( "attributes" );
+            foreach ($attribute as $attr) {
+                $xml_attri = $xml->createElement( "attribute", $attr->name);
+                $xml_attribute->appendChild( $xml_attri );
+                $attri = $attr->name;
+            }
+            $xml_product->appendChild( $xml_attribute );
+            
+            $xml_products->appendChild( $xml_product );
+            $xml->appendChild( $xml_products );
         }
 
         print $xml->saveXML();
