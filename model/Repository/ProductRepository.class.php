b05961b01875a28457c637ce9a5edbd362e42dd3
Harisankar <mrsank@gmail.com>
2015-08-11 15:15:10 +0530

    Modified the code for fetching the attribute

--- model/Repository/ProductRepository.class.php
+++ model/Repository/ProductRepository.class.php
@@ -15,6 +15,25 @@ class ProductRepository
      */
     public function getProductByCategory( $sCategoryName )
     {
+
+        /*
+         * @ Modified : Harisankar <mrsank@live.in>
+         * @ Date : 08/11/2015
+         * @ Purpose : Checking category name "Pizza" and fetching attributes accordingly
+         */
+
+        if($sCategoryName=="pizza" || $sCategoryName=="Pizza"){
+            return Model::factory('product')->raw_query('select
+            product.cat_id,product.name,product.cost, GROUP_CONCAT(attr.name) as `attribute`
+            from `attr`
+            join `attr2pizza` on attr2pizza.attr_id = attr.id
+            join `product` on attr2pizza.pizza_id = product.id
+            where attr.id = attr2pizza.attr_id and product.id = attr2pizza.pizza_id
+            group by attr2pizza.pizza_id')->find_many();
+        }
+
+        /* #Modification ends here... */
+        
         return Model::factory('product')
                     ->select('product.*')
                     ->join('categories', ['product.cat_id', '=', 'categories.id'])
@@ -39,7 +58,5 @@ class ProductRepository
             ->order_by_asc('pos')
             ->find_many();
     }
-
-
 
 }
